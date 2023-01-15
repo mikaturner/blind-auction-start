0c41e6c206985b6d2cc723234efd63a8a34afed5
Mika Turner <69220717+mikaturner@users.noreply.github.com>
2023-01-15 01:37:28 +0000

    Completed program so it runs and ends correctly

--- main.py
+++ main.py
@@ -4,31 +4,21 @@ import art
 print(art.logo)
 print("Welcome to the secret auction program.") 
 
-name = input("What is your name? ") 
-bid = input("What's your bid? ")
-bidders = input("Are there any other bidders? Type 'yes' or 'no'. ")
-
 auction_dic = {}
-
-def secret_auction(name, bid, bidders):
-  best_bid = 0  
+auction_open = True
+   
+while auction_open:
+  name = input("What is your name? ") 
+  bid = input("What's your bid? ")
+  bidders = input("Are there any other bidders? Type 'yes' or 'no'. ")
+  auction_dic[name] = float(bid.replace("$",""))
   if bidders == "yes":
     auction_open = True
+    clear()
   else:
     auction_open = False
-  
-  while auction_open:
-    auction_dic[name] = float(bid.replace("$",""))
-    print(auction_dic)
-    
-    name = input("What is your name? ") 
-    bid = input("What's your bid? ")
-    bidders = input("Are there any other bidders? Type 'yes' or 'no'. ")
-    clear()
-    secret_auction(name, bid, bidders)
-  
-  for bidder in auction_dic:
+    best_bid = 0 
+    for bidder in auction_dic:
       if auction_dic[bidder] > best_bid:
         best_bid = auction_dic[bidder]
-  print(f"The winner is {bidder} with a bid of {best_bid}.")
-secret_auction(name, bid, bidders)
+    print(f"The winner is {bidder} with a bid of ${best_bid}0.")
